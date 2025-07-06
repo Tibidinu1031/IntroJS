d5f2ada3cb6fc83620f3ebc1b05ecd28e6c4b6f6
Tibidinu1031 <tiberiusdinu95@gmail.com>
2025-07-06 15:19:43 +0300

    Initial commit for IntroJS

---
+++ script.js
@@ -0,0 +1,79 @@
+console.log(30);
+console.log();
+console.log(80);
+
+const nameT = "Tiberius Dinu";
+console.log("x is 30 years old");
+
+console.log("-------------------");
+console.log();
+
+//Opening line
+console.log('It was love at first sight.');
+/*
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+console.log();
+console.log("Plm pe gard");
+
+console.log("-------------------");
+console.log();
+
+console.log("JavaScript");
+console.log(2011);
+console.log("Woohoo! I love to code! #codecademy");
+console.log(20.49);
+
+console.log("-------------------");
+console.log();
+
+console.log(30 + 3.5);
+console.log();
+
+console.log(2025 - 1969);
+console.log();
+
+console.log(65 / 240);
+console.log();
+
+console.log(0.2708 * 100);
+
+console.log("-------------------");
+console.log();
+
+console.log("Hello" + "World");
+console.log("Hello" + " " + "World");
+
+console.log("-------------------");
+console.log();
+
+console.log('Teaching the world how to code'.length);
+console.log();
+
+const myName = "George Tiberius Dinu";
+
+console.log(myName.length);
+
+console.log("-------------------");
+console.log();
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+console.log("-------------------");
+console.log();
+
+console.log(Math.floor(Math.random() * 100));
+
+console.log(Math.ceil(43.8));
+
+console.log(Number.isInteger(2017));
+
