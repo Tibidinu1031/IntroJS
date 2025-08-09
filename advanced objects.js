ebede260e92816c4aeb863c454ec52ab4d11f780
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-08-09 15:17:33 +0300

    Add files via upload

--- advanced objects.js
+++ advanced objects.js
@@ -44,3 +44,42 @@ console.log(roboot.energyLevel);
 console.log("----------------")
 console.log()
 
+const r0bot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._numOfSensors = num
+    } else {
+      console.log(`Pass in a number that is greater than or equal to 0`)
+    }
+  }
+};
+
+roobot.numOfSensors = 100;
+console.log(r0bot.numOfSensors)
+
+console.log("----------------")
+console.log()
+
+const roobotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop")
+    }
+  }
+}
+
+const tinCan = roobotFactory('P-500', true)
+
+tinCan.beep();
