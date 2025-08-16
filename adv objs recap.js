f210d0bb22a4e903ffa3452b47876559389edcd6
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-08-16 14:00:52 +0300

    Add files via upload

--- adv objs recap.js
+++ adv objs recap.js
@@ -67,6 +67,49 @@ const robotttt = {
 
 console.log(robotttt.energyLevel)
 
+console.log("Advanced Objects Recap");
+console.log("----------------------");
+
+const rooboot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if (typeof num === 'number' && num >=0) {
+      this._numOfSensors = num
+    } else {
+      console.log ('Pass in a number that is greater than or equal to 0')
+    }
+  }
+};
+
+rooboot.numOfSensors = 100;
+console.log(rooboot.numOfSensors);
+
+console.log("Advanced Objects Recap");
+console.log("----------------------");
+
+const robotFactory = (model, mobile) => {
+  return {model: model,
+  mobile: mobile,
+  beep() {
+    console.log("Beep Boop")
+  }}
+  }
+
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
+
+
+
 
 
 
