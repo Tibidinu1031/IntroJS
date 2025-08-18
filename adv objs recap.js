97f18edb55724d3402a66264fc8132bc5e6ca69a
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-08-18 12:45:15 +0300

    Add files via upload

--- adv objs recap.js
+++ adv objs recap.js
@@ -108,8 +108,65 @@ const tinCan = robotFactory('P-500', true);
 
 tinCan.beep();
 
+console.log("Advanced Objects Recap");
+console.log("----------------------");
 
+const robotFactoryy = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
 
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactoryy('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
 
+console.log("Advanced Objects Recap");
+console.log("----------------------");
 
+const robottttt = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
 
+const {functionality} = robottttt;
+functionality.fireLaser()
+
+console.log("Advanced Objects Recap");
+console.log("----------------------");
+
+const robotttttt = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robotttttt);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robotttttt)
+
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobott = Object.assign({laserBlaster: true, voiceRecognition: true}, robotttttt)
+
+console.log(newRobott);
